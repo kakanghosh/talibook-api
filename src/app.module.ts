00c470a63eb07927be7f31cf8639bf70277988b3
kakanghosh <kakanghosh69@gmail.com>
2020-12-28 03:58:03 +0600

    refactore async typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { getConnectionOptions } from 'typeorm';
 
+async function typeORMConfig() {
+  const connectionOptions = await getConnectionOptions('default');
+  Object.assign(connectionOptions, {
+    entities: [`${__dirname}/**/entities/**/*{.ts,.js}`],
+    migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
+    subscribers: [`${__dirname}/subscribers/**/*{.ts,.js}`],
+    cli: {
+      entitiesDir: `${__dirname}/**/entities`,
+      migrationsDir: `${__dirname}/migrations`,
+      subscribersDir: `${__dirname}/subscribers`,
+    },
+  });
+  return connectionOptions;
+}
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: async () => {
-        const connectionOptions = await getConnectionOptions('default');
-        Object.assign(connectionOptions, {
-          entities: [`${__dirname}/**/entities/**/*{.ts,.js}`],
-          migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
-          subscribers: [`${__dirname}/subscribers/**/*{.ts,.js}`],
-          cli: {
-            entitiesDir: `${__dirname}/**/entities`,
-            migrationsDir: `${__dirname}/migrations`,
-            subscribersDir: `${__dirname}/subscribers`,
-          },
-        });
-        return connectionOptions;
-      },
+      useFactory: typeORMConfig,
     }),
     UserModule,
   ],
